47fc79d80952d784af3b0e963588b9960d37a26b
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-03-06 15:04:01 +0800

    Test

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+
+confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+recovery = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
 
-arts = pd.read_csv('/Users/jakirice/Desktop/art.csv')
-result = arts.loc[:,['Condition', 'ArtID']].groupby('Condition').size().reset_index(name='Number of Arts')
-result.sort_values(by=['Number of Arts'], inplace=True)
-con = result.iloc[-4:]
-colors = ["orange", "yellow", "lightblue", "pink"]
-explode = (0, 0, 0, 0.1)  
-plt.pie(con['Number of Arts'], labels=con['Condition'],explode = explode, colors=colors,
-autopct='%.1f%%', shadow=True, startangle=200)
-plt.title('Percentages of Art conditions')
-plt.show()
 
+def percent(a, b) : 
+  
+    result = int(((b - a) * 100) / a) 
+  
+    return result 
+  
+if __name__ == "__main__" : 
+  
+    a = int(input("Enter num1: "))
+    b = int(input("Enter num2: "))
+    
+    res = percent(a,b)
+    print(res, "%") 
+              
